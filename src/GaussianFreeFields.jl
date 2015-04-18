c94ae6c0c3e9975860c718905609ad5b897dcdaf
sswatson <samuel.s.watson@gmail.com>
2015-04-18 00:11:17 -0400

    Level line tools added, for working with the package Contour.jl

--- src/GaussianFreeFields.jl
+++ src/GaussianFreeFields.jl
@@ -1,7 +1,8 @@
 module GaussianFreeFields
 
 export DGFF,
-       fix_boundary_values
+       fix_boundary_values,
+       inside
 
 function DGFF(n::Int)
     h = complex(zeros(n,n));
@@ -50,5 +51,33 @@ function fix_boundary_values(h::Array{Float64,2},boundary_values::Array{Float64,
     return h - transpose(reshape(full(lufact(M) \ boundary),n,n))
 end
 
+# `inside` determines whether a point p is inside a curve γ
+# by checking the number of points of intersection between γ
+# and a ray emanating from p 
+function inside(p::Array{Float64,1},γ::Array{Float64,2})
+    if γ[1,:] != γ[length(γ[:,1]),:] 
+        return false
+    end
+    cntr = 0; m = sqrt(2); # the slope is an arbitrary irrational number
+    for i=1:length(γ[:,1])-1
+        (x1,y1,x2,y2) = (γ[i,1],γ[i,2],γ[i+1,1],γ[i+1,2])
+        if ((y2 - p[2] - m*(x2-p[1]))*(y1 - p[2] - m*(x1-p[1])) < 0) 
+            if (m*p[1]*x1 - p[2]*x1 - m*p[1]*x2 + p[2]*x2 - x2*y1 + x1*y2) / 
+                (m*x1 - m*x2 - y1 + y2)  - p[1] > 0
+                cntr += 1
+            end
+        end
+    end
+    return isodd(cntr)    
+end
+
+import Contour
+import Graphics2D.Line
+
+function Line(curve::Contour.Curve2{Float64};args...)
+    cv = curve.vertices
+    return Line(hcat(Float64[cv[k][1] for k=1:length(cv)], 
+    Float64[cv[k][2] for k=1:length(cv)]);args...)
+end
 
 end # module
