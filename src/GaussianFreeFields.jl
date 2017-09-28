b0a942a2953551b0f05d63d06480e81dc190fda1
Samuel S. Watson <samuel.s.watson@gmail.com>
2017-09-28 10:37:11 -0400

    add support for non-square rectangular domains

--- src/GaussianFreeFields.jl
+++ src/GaussianFreeFields.jl
@@ -13,24 +13,43 @@ export DGFF,
        flowline
 
 function DGFF(n::Int)
-    h = complex(zeros(n,n));
+    h = complex(zeros(n,n))
     for j=1:n
         for k=1:n
-            h[j,k] = 1/sqrt(2.0)*(j+k == 2 ? 0 : (randn() + im*randn()) * 1/sqrt(sin((j-1)*pi/n)^2+ sin((k-1)*pi/n)^2));
+            h[j,k] = 1/sqrt(2.0)*(j+k == 2 ? 0 : (randn() + im*randn()) * 1/sqrt(sin((j-1)*pi/n)^2+ sin((k-1)*pi/n)^2))
         end
     end
     return real(n*1/sqrt(2)*ifft(h))
 end
 
-function fix_boundary_values(h::Array{Float64,2},boundary_values::Array{Float64,2}=zeros(size(h)[1],size(h)[2]))
-    n = size(h)[1]
-    A = sparse([1,n],[1,n],[1,1.0])
+function DGFF(m::Int,n::Int)
+    h = complex(zeros(m,n))
+    for j=1:m
+        for k=1:n
+            h[j,k] = 1/sqrt(2.0)*(j+k == 2 ? 0 :
+                                  randn() + im*randn() *
+                                  1/sqrt(sin((j-1)*pi/m)^2+(sin((k-1)*pi/n))^2))
+        end
+    end
+    return real(n/sqrt(2)*ifft(h))
+end
+
+"Return the array obtained by subtracting from `h` the 
+harmonic extension of the values around the edge of 
+the array `boundary_values`"
+function fix_boundary_values(h::Array{Float64,2},
+                             boundary_values::Array{Float64,2}=zeros(h))
+    m,n = size(h)
+    #sparse matrix with ones in top left, lower right positions
+    cornersonly(m) = sparse([1,m],[1,m],[1.0,1.0])
+    #sparse matrix with ones along diagonal except top-left, lower-right
+    eyenocorners(n) = speye(n) - cornersonly(n)
+    A = cornersonly(m)
     B = speye(n)
-    C = B - A
     D_dense = zeros(n,n); 
     for i=1:n-1 
-        D_dense[i,i+1]=-1; 
-        D_dense[i+1,i]=-1; 
+        D_dense[i,i+1] = -1; 
+        D_dense[i+1,i] = -1; 
         D_dense[i,i] = 4; 
     end
     D_dense[1,1] = 1
@@ -38,25 +57,27 @@ function fix_boundary_values(h::Array{Float64,2},boundary_values::Array{Float64,
     D_dense[n,n-1] = 0
     D_dense[n,n] = 1
     D = sparse(D_dense);
-    E_dense = zeros(n,n)
-    for i=1:n-1 
+    E_dense = zeros(m,m)
+    for i=1:m-1 
         E_dense[i,i+1]=-1
         E_dense[i+1,i]=-1 
     end
-    E_dense[n,n-1] = 0
+    E_dense[m,m-1] = 0
     E_dense[1,2] = 0
     E = sparse(E_dense)
-    M = kron(A,B) + kron(C,D) + kron(E,C)
-    boundary = zeros(n^2)
-    for i in 1:n-1
-        boundary[i] = h[1,i] - boundary_values[1,i]
-        boundary[n^2-n+i] = h[n,i] - boundary_values[n,i]
+    Δ = kron(A,B) + kron(eyenocorners(m),D) + kron(E,eyenocorners(n))
+    boundary = zeros(m*n)
+    for i in 1:m-1
         boundary[n*i+1] = h[i+1,1] - boundary_values[i+1,1]
         boundary[n*i] = h[i,n] - boundary_values[i,n]
     end
-    boundary[n^2] = h[n,n]
+    for i in 1:n-1
+        boundary[i] = h[1,i] - boundary_values[1,i]
+        boundary[end-n+i] = h[m,i] - boundary_values[m,i]
+    end
+    boundary[end] = h[m,n]
     
-    return h - transpose(reshape(full(lufact(M) \ boundary),n,n))
+    return h - transpose(reshape(full(lufact(Δ) \ boundary),n,m))
 end
 
 function harmonicextension{T<:Number}(h::Array{T,2},vertices::Set)
